12748eafebcfdddea5df41000ce2e73e0245464e
pixelzoom <cmalley@pixelzoom.com>
2026-02-06 07:34:47 -0700

    panel for time controls

--- js/common/view/QBSTimeControl.ts
+++ js/common/view/QBSTimeControl.ts
@@ -6,25 +6,35 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
 import HBox from '../../../../scenery/js/layout/nodes/HBox.js';
+import Panel, { PanelOptions } from '../../../../sun/js/Panel.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import quantumBoundStates from '../../quantumBoundStates.js';
 import QuantumBoundStatesFluent from '../../QuantumBoundStatesFluent.js';
 import Time from '../model/Time.js';
+import QBSConstants from '../QBSConstants.js';
 import TimeButtonGroup from './TimeButtonGroup.js';
 
-export default class QBSTimeControl extends HBox {
+export default class QBSTimeControl extends Panel {
 
   public constructor( time: Time, tandem: Tandem ) {
 
-    const buttonGroup = new TimeButtonGroup( time, tandem.createTandem( 'buttonGroup' ) );
-
-    super( {
-      children: [ buttonGroup ],
-      spacing: 20,
+    const options = combineOptions<PanelOptions>( {}, QBSConstants.PANEL_OPTIONS, {
+      stroke: 'rgb( 200, 200, 200 )',
+      fill: null,
       accessibleHeading: QuantumBoundStatesFluent.a11y.timeControls.accessibleHeadingStringProperty,
       tandem: tandem
     } );
+
+    const buttonGroup = new TimeButtonGroup( time, tandem.createTandem( 'buttonGroup' ) );
+
+    const content = new HBox( {
+      children: [ buttonGroup ],
+      spacing: 20
+    } );
+
+    super( content, options );
   }
 }
 
