1a58c1660a15f360bc9e6814b633919702bd60c7
pixelzoom <cmalley@pixelzoom.com>
2026-02-06 07:46:18 -0700

    put tool checkboxes in a panel

--- js/common/view/ToolsCheckboxGroup.ts
+++ js/common/view/ToolsCheckboxGroup.ts
@@ -7,29 +7,40 @@
  */
 
 import Property from '../../../../axon/js/Property.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
 import VBox from '../../../../scenery/js/layout/nodes/VBox.js';
+import Panel, { PanelOptions } from '../../../../sun/js/Panel.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import quantumBoundStates from '../../quantumBoundStates.js';
+import QBSConstants from '../QBSConstants.js';
 import MagnifierToolCheckbox from './MagnifierToolCheckbox.js';
 import ReferenceLineCheckbox from './ReferenceLineCheckbox.js';
 
-export default class ToolsCheckboxGroup extends VBox {
+export default class ToolsCheckboxGroup extends Panel {
 
   public constructor( referenceLineVisibleProperty: Property<boolean>,
                       magnifierToolVisibleProperty: Property<boolean>,
                       tandem: Tandem ) {
 
+    const options = combineOptions<PanelOptions>( {}, QBSConstants.PANEL_OPTIONS, {
+      isDisposable: false,
+      stroke: null,
+      fill: null,
+      tandem: tandem,
+      phetioVisiblePropertyInstrumented: true
+    } );
+
     const magnifierToolCheckbox = new MagnifierToolCheckbox( referenceLineVisibleProperty, tandem.createTandem( 'magnifierToolCheckbox' ) );
 
     const referenceLineCheckbox = new ReferenceLineCheckbox( magnifierToolVisibleProperty, tandem.createTandem( 'referenceLineCheckbox' ) );
 
-    super( {
-      isDisposable: false,
+    const content = new VBox( {
       children: [ magnifierToolCheckbox, referenceLineCheckbox ],
       spacing: 6,
-      align: 'left',
-      tandem: tandem
+      align: 'left'
     } );
+
+    super( content, options );
   }
 }
 
