66c3fbf4e3c6ccdffbe67940184a80104a5e94dc
phet-dev <phetadmin@gmail.com>
2026-02-10 04:31:51 -0700

    update copyright dates from daily grunt work

--- js/common/model/femtosecondsUnit.ts
+++ js/common/model/femtosecondsUnit.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2026, University of Colorado Boulder
 
 //TODO https://github.com/phetsims/quantum-bound-states/issues/11 Move to scenery-phet/js/units/
 /**
