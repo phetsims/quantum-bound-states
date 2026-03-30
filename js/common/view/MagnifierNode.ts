3f7366093b5fdab2fd49b867b7e320143c3a8761
pixelzoom <cmalley@pixelzoom.com>
2026-03-30 16:00:47 -0600

    use distance for c1Offset calculation

--- js/common/view/MagnifierNode.ts
+++ js/common/view/MagnifierNode.ts
@@ -178,7 +178,7 @@ class MagnifierWireNode extends Path {
 
       // control points
       // The y coordinate of the body's control point varies with the x distance between the body and probe.
-      const c1Offset = new Vector2( 0, linear( 0, 800, 0, 200, bodyNode.centerX - probeNode.left ) ); // x distance -> y coordinate
+      const c1Offset = new Vector2( 0, linear( 0, 800, 0, 200, Math.abs( bodyNode.centerX - probeNode.left ) ) ); // x distance -> y coordinate
       const c2Offset = new Vector2( 50, 150 );
       const c1 = new Vector2( bodyConnectionPoint.x + c1Offset.x, bodyConnectionPoint.y + c1Offset.y );
       const c2 = new Vector2( probeConnectionPoint.x + c2Offset.x, probeConnectionPoint.y + c2Offset.y );
