2d47a997511242f3ecc2b8d4cacba86302f1e7e3
pixelzoom <cmalley@pixelzoom.com>
2026-03-31 15:26:30 -0600

    doc

--- js/common/QBSQueryParameters.ts
+++ js/common/QBSQueryParameters.ts
@@ -27,7 +27,7 @@ const QBSQueryParameters = QueryStringMachine.getAll( {
   // private - for internal use only
   //====================================================================================================================
 
-  // Runs a test at startup that compares NumerovSolver to analytical solutions. Output is in the browser console.
+  // Runs a test at startup that compares NumerovSolver to analytic solutions. Output is in the browser console.
   testNumerovSolver: {
     type: 'flag'
   },
