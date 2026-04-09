4fb0d36d242596b386857b7985f4c3b970f5d486
pixelzoom <cmalley@pixelzoom.com>
2026-04-09 13:56:36 -0600

    use Range.dilated to improve readability

--- js/common/QBSConstants.ts
+++ js/common/QBSConstants.ts
@@ -65,9 +65,9 @@ export default class QBSConstants {
   public static readonly ALL_GRAPHS_X_RANGE = new Range( -3.5, 3.5 );
   public static readonly ALL_GRAPHS_X_TICK_SPACING = 1;
   public static readonly ALL_GRAPHS_Y_AXIS_LABEL_X_OFFSET = -32;
-  public static readonly AVERAGE_PROBABILITY_DENSITY_GRAPH_Y_RANGE = new Range( -QBSConstants.ALL_GRAPHS_Y_PADDING, 1 + QBSConstants.ALL_GRAPHS_Y_PADDING );
+  public static readonly AVERAGE_PROBABILITY_DENSITY_GRAPH_Y_RANGE = new Range( 0, 1 ).dilated( QBSConstants.ALL_GRAPHS_Y_PADDING );
   public static readonly PROBABILITY_DENSITY_GRAPH_Y_RANGE = QBSConstants.AVERAGE_PROBABILITY_DENSITY_GRAPH_Y_RANGE;
-  public static readonly WAVEFUNCTION_GRAPH_Y_RANGE = new Range( -( 1 + QBSConstants.ALL_GRAPHS_Y_PADDING ), 1 + QBSConstants.ALL_GRAPHS_Y_PADDING );
+  public static readonly WAVEFUNCTION_GRAPH_Y_RANGE = new Range( -1, 1 ).dilated( QBSConstants.ALL_GRAPHS_Y_PADDING );
 
   // Energy Diagram
   public static readonly ENERGY_DIAGRAM_VIEW_HEIGHT = 265;
