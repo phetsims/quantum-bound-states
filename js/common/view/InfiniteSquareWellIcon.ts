0d5d41005d91e678141a5da24dd5270e8fff7bf0
pixelzoom <cmalley@pixelzoom.com>
2026-04-13 13:27:41 -0600

    remove arrows from Infinite Square and Infinite Step icons

--- js/common/view/InfiniteSquareWellIcon.ts
+++ js/common/view/InfiniteSquareWellIcon.ts
@@ -8,18 +8,14 @@
 
 import Shape from '../../../../kite/js/Shape.js';
 import optionize from '../../../../phet-core/js/optionize.js';
-import Node from '../../../../scenery/js/nodes/Node.js';
 import Path from '../../../../scenery/js/nodes/Path.js';
 import TColor from '../../../../scenery/js/util/TColor.js';
 import QBSColors from '../QBSColors.js';
 import QBSConstants from '../QBSConstants.js';
-import FiniteSquareWellsIcon from './FiniteSquareWellsIcon.js';
 
 type SelfOptions = {
   wellWidth: number;
   wellDepth: number;
-  arrowHeadWidth?: number;
-  arrowHeadHeight?: number;
   hasStep?: boolean;
   stroke?: TColor;
   lineWidth?: number;
@@ -27,63 +23,37 @@ type SelfOptions = {
 
 type InfiniteSquareWellIconOptions = SelfOptions;
 
-export default class InfiniteSquareWellIcon extends Node {
+export default class InfiniteSquareWellIcon extends Path {
 
   public constructor( providedOptions: InfiniteSquareWellIconOptions ) {
 
     const options = optionize<InfiniteSquareWellIconOptions, SelfOptions>()( {
 
       // SelfOptions
-      arrowHeadWidth: 6,
-      arrowHeadHeight: 4,
       hasStep: false,
       stroke: QBSColors.potentialEnergyColorProperty,
-      lineWidth: 2
+      lineWidth: QBSConstants.POTENTIAL_ICON_LINE_WIDTH
     }, providedOptions );
 
-    // Draw the well without the arrow heads, described from left to right, with or without a step.
-    let wellIcon: Node;
+    // Draw the well, described from left to right, with or without a step.
+    let shape: Shape;
     if ( options.hasStep ) {
-      const wellShape = new Shape()
+      shape = new Shape()
         .moveTo( 0, 0 )
         .lineTo( 0, options.wellDepth )
         .lineTo( options.wellWidth / 2, options.wellDepth )
         .lineTo( options.wellWidth / 2, options.wellDepth / 2 )
         .lineTo( options.wellWidth, options.wellDepth / 2 )
         .lineTo( options.wellWidth, 0 );
-      wellIcon = new Path( wellShape, {
-        stroke: QBSColors.potentialEnergyColorProperty,
-        lineWidth: QBSConstants.POTENTIAL_ICON_LINE_WIDTH
-      } );
     }
     else {
-      wellIcon = new FiniteSquareWellsIcon( {
-        numberOfWells: 1,
-        wellWidth: options.wellWidth,
-        wellDepth: options.wellDepth,
-        edgeLength: 0
-      } );
+      shape = new Shape()
+        .moveTo( 0, 0 )
+        .lineTo( 0, options.wellDepth )
+        .lineTo( options.wellWidth, options.wellDepth )
+        .lineTo( options.wellWidth, 0 );
     }
 
-    // Draw the arrow heads, starting at the tip of each.
-    const arrowHeadsShape = new Shape()
-      // Left arrow head
-      .moveTo( 0, -options.arrowHeadHeight )
-      .lineTo( -options.arrowHeadWidth / 2, 0 )
-      .lineTo( options.arrowHeadWidth / 2, 0 )
-      .close()
-      // Right arrow head
-      .newSubpath()
-      .moveTo( options.wellWidth, -options.arrowHeadHeight )
-      .lineTo( options.wellWidth - options.arrowHeadWidth / 2, 0 )
-      .lineTo( options.wellWidth + options.arrowHeadWidth / 2, 0 )
-      .close();
-    const arrowHeadsPath = new Path( arrowHeadsShape, {
-      fill: QBSColors.potentialEnergyColorProperty
-    } );
-
-    super( {
-      children: [ wellIcon, arrowHeadsPath ]
-    } );
+    super( shape, options );
   }
 }
