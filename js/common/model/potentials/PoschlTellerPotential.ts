dc8e854fd3a22a605ed782dce75e77537ab1c8ee
pixelzoom <cmalley@pixelzoom.com>
2026-04-15 15:30:28 -0600

    first attempt at Poschl-Teller getPotentialEnergyAt

--- js/common/model/potentials/PoschlTellerPotential.ts
+++ js/common/model/potentials/PoschlTellerPotential.ts
@@ -7,24 +7,44 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import optionize, { EmptySelfOptions } from '../../../../../phet-core/js/optionize.js';
+import Multilink from '../../../../../axon/js/Multilink.js';
+import NumberProperty from '../../../../../axon/js/NumberProperty.js';
+import Range from '../../../../../dot/js/Range.js';
+import optionize from '../../../../../phet-core/js/optionize.js';
+import { nanometersUnit } from '../../../../../scenery-phet/js/units/nanometersUnit.js';
 import Node from '../../../../../scenery/js/nodes/Node.js';
 import Text from '../../../../../scenery/js/nodes/Text.js';
 import QuantumBoundStatesFluent from '../../../QuantumBoundStatesFluent.js';
 import QBSColors from '../../QBSColors.js';
 import QBSConstants from '../../QBSConstants.js';
+import { electronVoltsUnit } from '../units/electronVoltsUnit.js';
 import QuantumPotential, { QuantumPotentialOptions } from './QuantumPotential.js';
 
-type SelfOptions = EmptySelfOptions;
+const ENERGY_AXIS_RANGE = new Range( -15.5, 5.5 );
+
+type SelfOptions = {
+  wellWidth?: number;
+  wellWidthRange?: Range;
+  //TODO spacing - This is problematic because width and spacing are related.
+};
 
 export type PoschlTellerPotentialOptions = SelfOptions &
   Pick<QuantumPotentialOptions, 'numberOfWellsProperty' | 'electricFieldProperty' | 'tandem'>;
 
 export default class PoschlTellerPotential extends QuantumPotential {
 
+  public readonly wellWidthProperty: NumberProperty;
+  public readonly wellDepthProperty: NumberProperty;
+
+  //TODO spacingProperty
+
   public constructor( providedOptions: PoschlTellerPotentialOptions ) {
 
     const options = optionize<PoschlTellerPotentialOptions, SelfOptions, QuantumPotentialOptions>()( {
+
+      // SelfOptions
+      wellWidth: QBSConstants.WELL_WIDTH_RANGE.defaultValue,
+      wellWidthRange: QBSConstants.WELL_WIDTH_RANGE,
 
       // QuantumPotentialOptions
       visualNameProperty: QuantumBoundStatesFluent.potentialWells.poschlTellerStringProperty,
@@ -32,13 +52,37 @@ export default class PoschlTellerPotential extends QuantumPotential {
     }, providedOptions );
 
     super( options );
+
+    this.wellWidthProperty = new NumberProperty( options.wellWidth, {
+      units: nanometersUnit,
+      range: options.wellWidthRange,
+      tandem: options.tandem.createTandem( 'wellWidthProperty' ),
+      phetioFeatured: true
+    } );
+
+    this.wellDepthProperty = new NumberProperty( QBSConstants.WELL_DEPTH_RANGE.defaultValue, {
+      units: electronVoltsUnit,
+      range: QBSConstants.WELL_DEPTH_RANGE,
+      tandem: options.tandem.createTandem( 'wellDepthProperty' ),
+      phetioFeatured: true
+    } );
+
+    // Changes to Properties instantiated by this class trigger notification.
+    //TODO add spacingProperty
+    Multilink.multilink(
+      [ this.wellWidthProperty, this.wellDepthProperty ],
+      () => this.propertyChangedEmitter.emit() );
   }
 
   /**
    * Gets the potential energy (eV) at a specified x-coordinate (nm).
    */
   public getPotentialEnergyAt( x: number ): number {
-    return 0; //TODO implement getPotentialEnergyAt: N wells, electricField, yOffset, xOffset, wellWidth, wellDepth, spacing?
+    //TODO add support for numberOfWells, electricField, yOffset, xOffset, spacing?
+    const V0 = this.wellDepthProperty.value;
+    const a = this.wellWidthProperty.value;
+    const coshVal = Math.cosh( x / a );
+    return -V0 / ( coshVal * coshVal );
   }
 
   /**
@@ -46,6 +90,13 @@ export default class PoschlTellerPotential extends QuantumPotential {
    */
   public override getGroundStateIndex(): number {
     return 0;
+  }
+
+  /**
+   * Gets the range of the energy axis (y-axis).
+   */
+  public override getEnergyAxisRange(): Range {
+    return ENERGY_AXIS_RANGE;
   }
 
   public override getMinPotentialEnergy(): number {
