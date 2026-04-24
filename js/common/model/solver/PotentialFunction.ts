9cbd5989f057932d4ea718fb8108500288d5aa92
veillettem <martin.veillette@gmail.com>
2026-04-24 11:41:43 -0400

    sync-to-poly: Add more PotentialFunction examples
    Mono-SHA: 8bba060cd00112e67f9d166b547d8f2a62d94f76

--- js/common/model/solver/PotentialFunction.ts
+++ js/common/model/solver/PotentialFunction.ts
@@ -30,5 +30,26 @@
  *   const V0 = 10; // 10 eV well depth;
  *   return Math.abs( x ) < L / 2 ? -V0 : 0;
  * };
+ * 
+ * @example
+ * // Double well: V(x) = -V₀ for |x ± d/2| < w/2, 0 otherwise
+ * const doubleWell = ( x: number ) => {   
+ *   const d = 1; // Separation between wells in nm
+ *   const w = 0.5; // Width of each well in nm
+ *   const V0 = 10; // Depth of each well in eV
+ *   const leftWell = Math.abs( x + d / 2 ) < w / 2 ? -V0 : 0; 
+ *   const rightWell = Math.abs( x - d / 2 ) < w / 2 ? -V0 : 0;
+ *   return leftWell + rightWell;
+ * 
+ * @example
+ * // Poschl-Teller potential: V(x) = -V₀ / cosh²( (x - x₀) / w )
+ * const poschlTeller = ( x: number ) => {   
+ *  const V0 = 10; // Depth of the well in eV
+ *  const x0 = 0; // Center of the well in nm
+ *  const w = 0.5; // Width parameter in nm
+ *  const coshValue = Math.cosh( ( x - x0 ) / w );  
+ *  return -V0 / ( coshValue * coshValue );
+ * };
+ * 
  */
 export type PotentialFunction = ( x: number ) => number;
