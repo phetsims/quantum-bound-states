4481b45843329d5300c1c2bf0fbb0ef1ee1c6fa7
Chris Malley <cmalley@pixelzoom.com>
2026-04-29 15:54:10 -0600

    sync-to-poly: TODO
    Mono-SHA: 53589d1c709e34ff645d221b345c220a6c8f9dc2

--- js/common/view/EnergyAxisDragHandle.ts
+++ js/common/view/EnergyAxisDragHandle.ts
@@ -1,5 +1,6 @@
 // Copyright 2026, University of Colorado Boulder
 
+//TODO Make this a child of EnergyDiagramNode and sort of coordinate transform problems.
 /**
  * EnergyAxisDragHandle is the drag handle used to change the y-offset of the selected potential and (as a side effect)
  * change the range of the y-axis for the Energy Diagram,
