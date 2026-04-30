fe3f2f3a0857b621f010885e9865fbf1a548f895
Chris Malley <cmalley@pixelzoom.com>
2026-04-30 11:14:51 -0600

    sync-to-poly: add sound behavior similar to Slider
    Mono-SHA: d90d4507068dad95fe4b8594112b0dd5a5dd40b9

--- js/common/view/EnergyAxisDragListener.ts
+++ js/common/view/EnergyAxisDragListener.ts
@@ -14,12 +14,13 @@ import Bounds2 from '../../../../dot/js/Bounds2.js';
 import { clamp } from '../../../../dot/js/util/clamp.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import Vector2Property from '../../../../dot/js/Vector2Property.js';
-import SoundRichDragListener from '../../../../scenery-phet/js/SoundRichDragListener.js';
+import RichDragListener from '../../../../scenery/js/listeners/RichDragListener.js';
+import ValueChangeSoundPlayer from '../../../../tambo/js/sound-generators/ValueChangeSoundPlayer.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import QuantumPotential from '../model/potentials/QuantumPotential.js';
 import EnergyAxisDragHandle from './EnergyAxisDragHandle.js';
 
-export default class EnergyAxisDragListener extends SoundRichDragListener {
+export default class EnergyAxisDragListener extends RichDragListener {
 
   public constructor( energyAxisDragHandle: EnergyAxisDragHandle,
                       potentialProperty: TReadOnlyProperty<QuantumPotential>,
@@ -37,6 +38,12 @@ export default class EnergyAxisDragListener extends SoundRichDragListener {
       energyDiagramRectangleBounds.minX,
       energyDiagramRectangleBounds.maxY ) );
 
+    // Use the range of yOffsetProperty for the selected potential to determine the sound range.
+    let soundPlayer: ValueChangeSoundPlayer;
+    potentialProperty.link( potential => {
+      soundPlayer = new ValueChangeSoundPlayer( potential.yOffsetProperty.range );
+    } );
+
     super( {
       tandem: tandem,
       positionProperty: positionProperty,
@@ -50,10 +57,17 @@ export default class EnergyAxisDragListener extends SoundRichDragListener {
       },
 
       drag: ( event, listener ) => {
+
+        const previousYOffset = potentialProperty.value.yOffsetProperty.value;
+
+        // Compute the new yOffset value.
         const dy = energyDiagramChartTransform.viewToModelDeltaY( listener.modelDelta.y );
         let yOffset = potentialProperty.value.yOffsetProperty.value - dy;
         yOffset = clamp( yOffset, potentialProperty.value.yOffsetProperty.range.min, potentialProperty.value.yOffsetProperty.range.max );
         potentialProperty.value.yOffsetProperty.value = yOffset;
+
+        // Play sound to communicate how yOffset changed, similar to Slider behavior.
+        soundPlayer.playSoundForValueChange( yOffset, previousYOffset );
       },
 
       end: ( event, listener ) => energyAxisDragHandle.describeMoved()
