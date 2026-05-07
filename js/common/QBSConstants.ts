0d4e52d21a778302482caa91705a79c9b05e61db
Chris Malley <cmalley@pixelzoom.com>
2026-05-06 20:16:17 -0600

    sync-to-poly: use monospace font for time display so that it doesn't shift around
    Mono-SHA: 04802add4742749a108f409d87c4b8a63e6586ae

--- js/common/QBSConstants.ts
+++ js/common/QBSConstants.ts
@@ -57,7 +57,7 @@ export default class QBSConstants {
   public static readonly TICK_LABEL_FONT = new PhetFont( 12 ); // for tick labels on graphs
   public static readonly CONTROL_FONT = new PhetFont( 14 ); // for text on checkboxes, radio buttons, push buttons, etc.
   public static readonly TITLE_FONT = new PhetFont( { size: 14, weight: 'bold' } );
-  public static readonly TIME_FONT = new PhetFont( 16 );
+  public static readonly TIME_FONT = new PhetFont( { size: 16, family: 'monospace' } );
   public static readonly EQUATION_TERM_FONT = new PhetFont( 18 );
 
   // Energy Diagram & all Quantum State Graphs
