f2db8b5a058b931618b1f38320157dc9af47e8bf
veillettem <martin.veillette@gmail.com>
2026-05-28 15:15:47 -0400

    sync-to-poly: type-check
    Mono-SHA: 74c67584d2e573fd83177428b3e3a6724ce251c3

--- js/common/model/QBSSolverTests.ts
+++ js/common/model/QBSSolverTests.ts
@@ -860,7 +860,7 @@ QUnit.test( 'mass sweep', assert => {
 
   for ( const mass of SWEEP_MASSES ) {
     const potFn = CoulombSolution.createPotentialFunction( {
-      numberOfWells: 1, xOffset: 0, yOffset: yOffset, electricField: 0, coupling: 1.44
+      numberOfWells: 1, xOffset: 0, yOffset: yOffset, electricField: 0
     } );
 
     configs.push( {
@@ -1378,13 +1378,13 @@ QUnit.test( 'energy error < 1% for default Coulomb', assert => {
   const massC = 1;
   const gridC = standardGrid();
   const potFnC = CoulombSolution.createPotentialFunction( {
-    numberOfWells: 1, xOffset: 0, yOffset: 0, electricField: 0, coupling: 1.44
+    numberOfWells: 1, xOffset: 0, yOffset: 0, electricField: 0
   } );
 
   const numericalResultC = NumerovSolver.solve( gridC, potFnC, massC, -17.5, 0 );
   const analyticalResultC = CoulombSolution.solve( gridC, {
     numberOfWells: 1, xOffset: 0, yOffset: 0,
-    energyMin: -17.5, energyMax: 0, electronMasses: massC, electricField: 0, coupling: 1.44
+    energyMin: -17.5, energyMax: 0, electronMasses: massC, electricField: 0
   } );
 
   // The standard 3001-point grid cannot resolve Coulomb excited states (Bohr radius
