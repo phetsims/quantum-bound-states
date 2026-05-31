d9416bf0631b6119b6f88d7d398351e087118853
Chris Malley <cmalley@pixelzoom.com>
2026-05-31 19:56:20 -0400

    sync-to-poly: TODO
    Mono-SHA: c499bcb903c4373c24c0372e5a09504145c8cd0c

--- js/common/view/handles/FiniteSquareDepthDragListener.ts
+++ js/common/view/handles/FiniteSquareDepthDragListener.ts
@@ -36,6 +36,7 @@ export default class FiniteSquareDepthDragListener extends PotentialDragListener
       keyboardShiftDragDelta: 0.1, // eV
 
       // Since we are not providing options.transform, dragBoundsProperty is in view coordinates.
+      //TODO https://github.com/phetsims/quantum-bound-states/issues/53 dragBoundsProperty is incorrect, does not account for electric field.
       dragBoundsProperty: new DerivedProperty( [ potential.yOffsetProperty ],
         yOffset => new Bounds2(
           energyDiagramRectangleBounds.minX,
