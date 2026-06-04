689239a2f0fa603e24a4720242f83a8fa53fc6ce
Chris Malley <cmalley@pixelzoom.com>
2026-06-04 10:58:54 -0400

    sync-to-poly: fix Poschl-Teller width marker for multiple wells, https://github.com/phetsims/quantum-bound-states/issues/53
    Mono-SHA: 8dcd6e4148ea9a8887cd4c883ba019e7d0bbb659

--- js/common/view/markers/PoschlTellerWidthMarkerNode.ts
+++ js/common/view/markers/PoschlTellerWidthMarkerNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2026, University of Colorado Boulder
 
-//TODO https://github.com/phetsims/quantum-bound-states/issues/53 identical to CoulombWidthMarkerNode
 /**
  * PoschlTellerWidthMarkerNode is the marker (pair of vertical dashed lines) that shows the well width of a Poschl-Teller potential.
  *
@@ -10,11 +9,10 @@
 import ChartTransform from '../../../../../bamboo/js/ChartTransform.js';
 import Shape from '../../../../../kite/js/Shape.js';
 import Tandem from '../../../../../tandem/js/Tandem.js';
-import MorsePotential from '../../model/potentials/MorsePotential.js';
 import PoschlTellerPotential from '../../model/potentials/PoschlTellerPotential.js';
 import PotentialMarkerNode from './PotentialMarkerNode.js';
 
-export default class PoschlTellerWidthMarkerNode extends PotentialMarkerNode<MorsePotential> {
+export default class PoschlTellerWidthMarkerNode extends PotentialMarkerNode<PoschlTellerPotential> {
 
   public constructor( potential: PoschlTellerPotential,
                       chartTransform: ChartTransform,
@@ -29,7 +27,7 @@ export default class PoschlTellerWidthMarkerNode extends PotentialMarkerNode<Mor
    * Update the 2 vertical lines.
    */
   protected override update(): void {
-    const xMax = this.chartTransform.modelToViewX( this.potential.xOffsetProperty.value + this.potential.wellWidthProperty.value / 2 );
+    const xMax = this.chartTransform.modelToViewX( this.potential.xOffsetProperty.value + this.potential.getTotalWidth() / 2 );
     const xMin = xMax - this.chartTransform.modelToViewDeltaX( this.potential.wellWidthProperty.value );
     const yMin = 0;
     const yMax = this.chartTransform.viewHeight;
