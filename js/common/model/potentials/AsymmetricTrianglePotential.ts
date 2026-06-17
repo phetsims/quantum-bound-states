3ba2152e61440f97d442dff0b842b3e6b6ff1059
Chris Malley <cmalley@pixelzoom.com>
2026-06-17 11:36:33 -0600

    sync-to-poly: reduce range of well depth for AsymmetricTrianglePotential so that width drag handle stays in chart bounds
    Mono-SHA: d94c8ebd1fcba848dbd42b840898465ff90cd30a

--- js/common/model/potentials/AsymmetricTrianglePotential.ts
+++ js/common/model/potentials/AsymmetricTrianglePotential.ts
@@ -33,7 +33,7 @@ export default class AsymmetricTrianglePotential extends QuantumPotentialDepth {
 
       // QuantumPotentialOptions
       wellWidthRange: new RangeWithValue( 0.5, 6, 1 ), // for 1 well
-      wellDepthRange: new RangeWithValue( 1, 20, 10 ), // for 1 well
+      wellDepthRange: new RangeWithValue( 1, 18, 10 ), // for 1 well
       visualNameProperty: QuantumBoundStatesFluent.potentialWells.asymmetricTriangleStringProperty,
       tandemPrefix: 'asymmetricTrianglePotential'
     }, providedOptions );
