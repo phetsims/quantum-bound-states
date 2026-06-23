cb9a49542a03645ab137a5e7c2f5b5f690350d33
Chris Malley <cmalley@pixelzoom.com>
2026-06-23 17:06:43 -0600

    sync-to-poly: lower Poschl-Teller spacing handle because it no longer conflicts with width handle
    Mono-SHA: e79e00e57633fbb959e379245fcbec274bd0d2d9

--- js/common/view/handles/PoschlTellerSpacingHandleNode.ts
+++ js/common/view/handles/PoschlTellerSpacingHandleNode.ts
@@ -20,7 +20,7 @@ import PotentialHandleNode from './PotentialHandleNode.js';
 
 // The separation handle will be placed this many eV above the potential's energy offset.
 // Vertical marker lines indicate where the separation is measured.
-const ENERGY_OFFSET = 3; // eV
+const ENERGY_OFFSET = 1; // eV
 
 export default class PoschlTellerSpacingHandleNode extends PotentialHandleNode<PoschlTellerPotential> {
 
