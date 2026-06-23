d7bbd867561e0c9683e74005e3554e2edf8f416d
Chris Malley <cmalley@pixelzoom.com>
2026-06-23 13:48:23 -0600

    sync-to-poly: move separation handle up a bit, to minimize overlap with width handle, https://github.com/phetsims/quantum-bound-states/issues/90
    Mono-SHA: 1b5188c5f0c1939dd116c6e8b6a687cc620cc906

--- js/common/view/handles/FiniteSquareSeparationHandleNode.ts
+++ js/common/view/handles/FiniteSquareSeparationHandleNode.ts
@@ -20,7 +20,7 @@ import PotentialHandleNode from './PotentialHandleNode.js';
 
 // The separation handle will be placed this many eV above the potential's energy offset.
 // Vertical marker lines indicate where the separation is measured.
-const ENERGY_OFFSET = 1.5; // eV
+const ENERGY_OFFSET = 2.75; // eV
 
 export default class FiniteSquareSeparationHandleNode extends PotentialHandleNode<FiniteSquarePotential> {
 
