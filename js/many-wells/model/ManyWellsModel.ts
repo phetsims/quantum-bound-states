20e43c637518d28a6b04b5c56cfa29a71af135c0
brettfiedler <brett.fiedler@colorado.edu>
2026-06-24 14:23:00 -0400

    sync-to-poly: adjust default many wells parameters https://github.com/phetsims/quantum-bound-states/issues/69
    Mono-SHA: f1c3b6b431ecf5f54326eb6f7bc6357b29a699a1

--- js/many-wells/model/ManyWellsModel.ts
+++ js/many-wells/model/ManyWellsModel.ts
@@ -54,9 +54,9 @@ export default class ManyWellsModel extends QBSModel {
         numberOfWellsProperty: numberOfWellsProperty,
         electronMassesProperty: electronMassesProperty,
         electricFieldProperty: electricFieldProperty,
-        wellWidthRange: new RangeWithValue( 0.1, 0.5, 0.5 ),
+        wellWidthRange: new RangeWithValue( 0.1, 0.5, 0.45 ),
         wellWidthDecimalPlaces: QBSConstants.WELL_WIDTH_DECIMAL_PLACES_MANY_WELLS,
-        wellDepthRange: new RangeWithValue( 5, 15, 10 ),
+        wellDepthRange: new RangeWithValue( 5, 15, 9 ),
         separationRange: new RangeWithValue( 0.05, 0.25, 0.1 ),
         tandem: potentialsTandem.createTandem( 'finiteSquarePotential' )
       } ),
@@ -65,8 +65,8 @@ export default class ManyWellsModel extends QBSModel {
         electronMassesProperty: electronMassesProperty,
         electricFieldProperty: electricFieldProperty,
         wellWidthRange: new RangeWithValue( 0.2, 0.2, 0.2 ), // effectively constant
-        wellDepthRange: new RangeWithValue( 5, 20, 10 ),
-        spacingRange: new RangeWithValue( 0.3, 0.7, 0.5 ),
+        wellDepthRange: new RangeWithValue( 5, 15, 10 ),
+        spacingRange: new RangeWithValue( 0.3, 0.8, 0.5 ),
         tandem: potentialsTandem.createTandem( 'poschlTellerPotential' )
       } )
     ];
