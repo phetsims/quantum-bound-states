7a789cd3338badca6aeefb0efa7091cf692e76ae
Chris Malley <cmalley@pixelzoom.com>
2026-06-25 16:04:34 -0600

    sync-to-poly: implement MultiColumnKeyboardHelpContent, reimplement TwoColumnKeyboardHelpContent as a subclass, use in QBS, https://github.com/phetsims/scenery-phet/issues/983
    Mono-SHA: e06030730289c962b5b8ced40a6d476c8fc04621

--- js/common/view/QBSKeyboardHelpContent.ts
+++ js/common/view/QBSKeyboardHelpContent.ts
@@ -12,76 +12,57 @@ import MoveDraggableItemsKeyboardHelpSection from '../../../../scenery-phet/js/k
 import SliderControlsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/SliderControlsKeyboardHelpSection.js';
 import SpinnerControlsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/SpinnerControlsKeyboardHelpSection.js';
 import TimeControlsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/TimeControlsKeyboardHelpSection.js';
-import HBox from '../../../../scenery/js/layout/nodes/HBox.js';
-import VBox from '../../../../scenery/js/layout/nodes/VBox.js';
 import QuantumBoundStatesFluent from '../../QuantumBoundStatesFluent.js';
+import MultiColumnKeyboardHelpContent from '../../../../scenery-phet/js/keyboard/help/MultiColumnKeyboardHelpContent.js';
 import ReferenceLineKeyboardHelpSection from './ReferenceLineKeyboardHelpSection.js';
 
-const ROW_SPACING = 40;
-const COLUMN_SPACING = 40;
-
-export default class QBSKeyboardHelpContent extends HBox {
+export default class QBSKeyboardHelpContent extends MultiColumnKeyboardHelpContent {
 
   public constructor() {
 
-    const column1 = new VBox( {
-      align: 'left',
-      spacing: ROW_SPACING,
-      children: [
+    const column1 = [
 
-        // Potential Handles
-        new MoveDraggableItemsKeyboardHelpSection( {
-          headingStringProperty: QuantumBoundStatesFluent.keyboardHelp.potentialHandles.headingStringProperty
-        } ),
+      // Potential Handles
+      new MoveDraggableItemsKeyboardHelpSection( {
+        headingStringProperty: QuantumBoundStatesFluent.keyboardHelp.potentialHandles.headingStringProperty
+      } ),
 
-        // Magnifier
-        new MoveDraggableItemsKeyboardHelpSection( {
-          headingStringProperty: QuantumBoundStatesFluent.keyboardHelp.magnifier.headingStringProperty
-        } ),
+      // Magnifier
+      new MoveDraggableItemsKeyboardHelpSection( {
+        headingStringProperty: QuantumBoundStatesFluent.keyboardHelp.magnifier.headingStringProperty
+      } ),
 
-        // Reference Line
-        new ReferenceLineKeyboardHelpSection()
-      ]
-    } );
+      // Reference Line
+      new ReferenceLineKeyboardHelpSection()
+    ];
 
-    const column2 = new VBox( {
-      align: 'left',
-      spacing: ROW_SPACING,
-      children: [
+    const column2 = [
 
-        // Time Controls
-        new TimeControlsKeyboardHelpSection(),
+      // Time Controls
+      new TimeControlsKeyboardHelpSection(),
 
-        // Combo Boxes
-        new ComboBoxKeyboardHelpSection( {
-          headingString: QuantumBoundStatesFluent.keyboardHelp.comboBox.headingStringProperty
-        } ),
+      // Combo Boxes
+      new ComboBoxKeyboardHelpSection( {
+        headingString: QuantumBoundStatesFluent.keyboardHelp.comboBox.headingStringProperty
+      } ),
 
-        // Slider Controls
-        new SliderControlsKeyboardHelpSection()
-      ]
-    } );
+      // Slider Controls
+      new SliderControlsKeyboardHelpSection()
+    ];
 
-    const column3 = new VBox( {
-      align: 'left',
-      spacing: ROW_SPACING,
-      children: [
+    const column3 = [
 
-        // Spinner Controls
-        new SpinnerControlsKeyboardHelpSection(),
+      // Spinner Controls
+      new SpinnerControlsKeyboardHelpSection(),
 
-        // Basic Actions
-        new BasicActionsKeyboardHelpSection( {
-          withCheckboxContent: true
-        } )
-      ]
-    } );
+      // Basic Actions
+      new BasicActionsKeyboardHelpSection( {
+        withCheckboxContent: true
+      } )
+    ];
 
-    super( {
-      isDisposable: false,
-      spacing: COLUMN_SPACING,
-      align: 'top',
-      children: [ column1, column2, column3 ]
+    super( [ column1, column2, column3 ], {
+      isDisposable: false
     } );
   }
 }
