c7111bd0f81c0d78ed2a3c4521f93d653e6f7e3b
Chris Malley <cmalley@pixelzoom.com>
2026-06-25 10:13:22 -0600

    sync-to-poly: update IOType documentation
    Mono-SHA: b39b123a1a5f11749924638e8f26bbd230c88b77

--- js/common/model/TimeEvolvedSuperposition.ts
+++ js/common/model/TimeEvolvedSuperposition.ts
@@ -24,6 +24,6 @@ export type TimeEvolvedSuperposition = CoreRecord<typeof SCHEMA>;
 
 // PhET-iO data-type serialization for TimeEvolvedSuperposition instances.
 export const TimeEvolvedSuperpositionIO = new SchemaOrientedIOType<TimeEvolvedSuperposition, typeof SCHEMA>( 'TimeEvolvedSuperpositionIO', {
-  documentation: 'Serialization for the time-evolved superposition of a wave function at a given energy level.',
-  stateSchema: SCHEMA
+  stateSchema: SCHEMA,
+  documentation: 'Time-evolved superposition of a wave function at the selected energy level.'
 } );
