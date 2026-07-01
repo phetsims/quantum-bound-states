b3e0c5075310b56c82e1ee379059977fb11eefee
Chris Malley <cmalley@pixelzoom.com>
2026-07-01 16:08:24 -0600

    sync-to-poly: rewrite TimeDisplay
    Mono-SHA: 9ccd9e42db1ccb9d03ab8176d645a9d2f52ba1cc

--- js/common/view/TimeDisplay.ts
+++ js/common/view/TimeDisplay.ts
@@ -3,57 +3,72 @@
 /**
  * TimeDisplay displays the current time, with a toggle button to show/hide the value.
  *
+ * Note that this implementation does not use NumberDisplay because we could not find a suitable workaround for a
+ * problem that is specific to this sim. When timeSpeedProperty changes, that changes the number of decimal places
+ * in the time value, which results in the NumberDisplay resizing.
+ *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
 import DerivedStringProperty from '../../../../axon/js/DerivedStringProperty.js';
-import Range from '../../../../dot/js/Range.js';
 import { toFixed } from '../../../../dot/js/util/toFixed.js';
-import NumberDisplay from '../../../../scenery-phet/js/NumberDisplay.js';
 import { femtosecondsUnit } from '../../../../scenery-phet/js/units/femtosecondsUnit.js';
+import Node from '../../../../scenery/js/nodes/Node.js';
+import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
+import Text from '../../../../scenery/js/nodes/Text.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import QuantumBoundStatesFluent from '../../QuantumBoundStatesFluent.js';
 import QBSTime from '../model/QBSTime.js';
 import QBSColors from '../QBSColors.js';
 import QBSConstants from '../QBSConstants.js';
 
-// This range typically determines the width of the display. If you make this larger, you may need to adjust
-// options.minBackgroundWidth. To test: pause the sim, step through all time speeds, and see if the time display resizes.
-const TIME_RANGE = new Range( 0, 100000 );
+const X_MARGIN = 8;
+const Y_MARGIN = 2;
+const TEXT_MAX_WIDTH = 80;
+const DISPLAY_WIDTH = TEXT_MAX_WIDTH + 2 * X_MARGIN;
+const DISPLAY_HEIGHT = new Text( 'X', { font: QBSConstants.TIME_FONT } ).height + 2 * Y_MARGIN;
 
-export default class TimeDisplay extends NumberDisplay {
+export default class TimeDisplay extends Node {
 
   public constructor( time: QBSTime, tandem: Tandem ) {
 
+    // Change the background fill based on whether the time value is visible.
     const backgroundFillProperty = time.timeVisibleProperty.derived(
       timeVisible => timeVisible ? QBSColors.timeDisplayEnabledFillProperty.value : QBSColors.timeDisplayDisabledFillProperty.value );
 
-    // For selecting the correct accessible paragraph via Fluent select_.
-    const timeStateProperty = new DerivedStringProperty( [ time.timeVisibleProperty, time.isPlayingProperty ],
-      ( timeVisible, isPlaying ) => !timeVisible ? 'isHidden' : isPlaying ? 'isPlaying' : 'isPaused' );
+    const backgroundNode = new Rectangle( 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, {
+      fill: backgroundFillProperty,
+      stroke: 'lightGray'
+    } );
+
+    // Format the time value with the number of decimal places that matches the time speed.
+    const timeStringProperty = new DerivedStringProperty( [ time.currentTimeProperty, time.timeSpeedProperty ],
+      ( currentTime, timeSpeed ) => femtosecondsUnit.getVisualSymbolPatternString( currentTime, {
+        decimalPlaces: time.getDecimalPlaces(),
+        showTrailingZeros: true
+      } ) );
+
+    const text = new Text( timeStringProperty, {
+      font: QBSConstants.TIME_FONT,
+      maxWidth: TEXT_MAX_WIDTH,
+      visibleProperty: time.timeVisibleProperty // Show/hide the time value.
+    } );
+
+    // Right-justify the time value.
+    text.localBoundsProperty.link( () => {
+      text.right = backgroundNode.right - X_MARGIN;
+      text.bottom = backgroundNode.bottom - Y_MARGIN;
+    } );
 
     const accessibleParagraphProperty = QuantumBoundStatesFluent.a11y.timeControls.numberDisplay.accessibleParagraph.createProperty( {
-      timeState: timeStateProperty,
+      // See select_timeState
+      timeState: new DerivedStringProperty( [ time.timeVisibleProperty, time.isPlayingProperty ],
+        ( timeVisible, isPlaying ) => !timeVisible ? 'isHidden' : isPlaying ? 'isPlaying' : 'isPaused' ),
       time: time.currentTimeProperty.derived( currentTime => toFixed( currentTime, time.getDecimalPlaces() ) )
     } );
 
-    super( time.currentTimeProperty, TIME_RANGE, {
-      isDisposable: false,
-      textOptions: {
-        visibleProperty: time.timeVisibleProperty, // Hide the time value.
-        font: QBSConstants.TIME_FONT
-      },
-      backgroundFill: backgroundFillProperty,
-      numberFormatter: value => femtosecondsUnit.getVisualSymbolPatternString( value, {
-        decimalPlaces: time.getDecimalPlaces(),
-        showTrailingZeros: true
-      } ),
-
-      // Display correct number of decimal places for timeSpeedProperty.
-      numberFormatterDependencies: [ time.timeSpeedProperty ],
-
-      // Caution! minBackgroundWidth must be large enough to prevent resizing when timeSpeedProperty changes.
-      minBackgroundWidth: 130,
+    super( {
+      children: [ backgroundNode, text ],
       accessibleParagraph: accessibleParagraphProperty,
       tandem: tandem.createTandem( 'valueDisplay' )
     } );
